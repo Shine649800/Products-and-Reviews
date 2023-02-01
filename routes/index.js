8ea1be7f4018674094dc43f98a816c2e7adc3788
zu3x <zu3x@fresnou.org>
2023-02-01 11:36:41 -0800

    Second Commit, slide 44

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const productController = require('../controllers/productController.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/products', productController.viewProducts);
+router.get('/products/profile/:id', productController.renderProfile);
+router.get('/products/edit/:id', productController.renderEditForm);
+router.get('/products/add', productController.renderAddForm);
+router.get('/products/delete/:id', productController.deleteProduct);
+router.post('/products/edit/:id', productController.updateProduct);
+router.post('/products/add', productController.addProduct);
 module.exports = router;
