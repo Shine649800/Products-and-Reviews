d7784281dbf65720f01b07519a4b0ca105a8ba04
zu3x <zu3x@fresnou.org>
2023-02-03 11:36:13 -0800

    Fourth commit, slide 12, 2nd powerpoint

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const productController = require('../controllers/productController.js')
-
+const reviewController = require('../controllers/reviewController')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -13,4 +13,10 @@ router.get('/products/add', productController.renderAddForm);
 router.get('/products/delete/:id', productController.deleteProduct);
 router.post('/products/edit/:id', productController.updateProduct);
 router.post('/products/add', productController.addProduct);
+
+router.get('/products/:productId/review/add', reviewController.renderAddReviewForm);
+router.post('/products/:productId/review/add', reviewController.addReview);
+router.get('/reviews/edit/:id', reviewController.renderEditReviewForm);
+router.post('/reviews/edit/:id', reviewController.updateReview);
+router.get('/reviews/delete/:id', reviewController.deleteReview);
 module.exports = router;
